adbeda8d0cb49b4ed4cfabd7c7a95d663bfaa16f
Valelu <valerialucia1998@gmail.com>
2025-10-02 16:21:16 -0300

    settings

--- Adoptapp/urls.py
+++ Adoptapp/urls.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
 from pets import views
+from django.conf import settings
+
+
 
 urlpatterns = [
     path("", include("users.urls")),
     path("mascotas/", include("pets.urls")),
     path("", views.descubrir_mascotas, name="home"),
+    
 ]
 
+if settings.DEBUG:
+    urlpatterns += [path("admin/", admin.site.urls)]
 
